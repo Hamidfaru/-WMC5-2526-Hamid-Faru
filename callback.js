9deb8e0c15aca43da5e7e0e0c0ab122419bbf558
Hamidfaru <137474320+Hamidfaru@users.noreply.github.com>
2026-02-06 22:24:34 +0100

    Javascript Nuggets - Callback Functions

---
+++ callback.js
@@ -0,0 +1,22 @@
+function makeUppercase(value){
+    console.log(value.UpperCase())
+}
+
+function reversString(Value){
+    console(value.split('').reverse().johin(''))
+}
+
+
+function handleNmae(name,cb){
+
+const fullName='${name} Faru'
+cb(fullName)
+}
+//handleNmae('Hamid',makeUppercase)
+handleNmae('Susan',(value)=>console.log(value))
+
+const btn= document.querySelector('.btn');
+btn.addEventListener('click'),function(){
+    console.log('Hello word')
+
+}
